b3270201b8d9435cb1c28c0ac28623c187392e9f
niklasbaier <niklas.baier@yahoo.de>
2024-06-10 08:10:36 +0200

    refactor: move BaseLinearRegression in its own file

--- src/tinyML/regressors/__init__.py
+++ src/tinyML/regressors/__init__.py
@@ -1,3 +1,4 @@
+from tinyML.regressors.base import BaseLinearRegression
 from tinyML.regressors.linear_regression import LinearRegressionGD, LinearRegressionOLS
 
-__all__ = ["LinearRegressionGD", "LinearRegressionOLS"]
+__all__ = ["BaseLinearRegression", "LinearRegressionGD", "LinearRegressionOLS"]
--- src/tinyML/regressors/linear_regression.py
+++ src/tinyML/regressors/linear_regression.py
@@ -4,33 +4,10 @@ import typing as t
 import matplotlib.pyplot as plt
 
 from tinyML import commons
+from tinyML.regressors import BaseLinearRegression
 
 commons.setup_logging()
 logger = logging.getLogger(__name__)
-
-
-class BaseLinearRegression:
-    def __init__(self):
-        self.weights = None
-        self.bias = None
-
-    def fit(self, X, y_true):
-        raise NotImplementedError("Method must be implemented in subclass.")
-
-    def predict(self, X):
-        if self.weights is None or self.bias is None:
-            raise ValueError("Model has not been trained yet.")
-        return np.dot(X, self.weights) + self.bias
-
-    def visualize_regression_line(self, X, y_truth):
-        plt.scatter(X, y_truth, color="blue", label="Data Points")
-        y_pred = self.predict(X)
-        plt.plot(X, y_pred, color="red", label="Regression Line")
-        plt.xlabel("X")
-        plt.ylabel("y")
-        plt.title("Linear Regression Fit")
-        plt.legend()
-        plt.show()
 
 
 class LinearRegressionGD(BaseLinearRegression):
