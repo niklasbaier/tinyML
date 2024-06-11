a1c183c6a86544e520164ea9e26a76c83e870c8f
niklasbaier <niklas.baier@yahoo.de>
2024-06-11 09:00:08 +0200

    chore: make integration tests allowed to fail in CICD

--- tests/integration/test_ensembles.py
+++ tests/integration/test_ensembles.py
@@ -13,9 +13,7 @@ def test_decision_tree(sample_data_classification):
     y_pred = model.predict(X_test)
 
     # scikit-learn model
-    sklearn_model = tree.DecisionTreeClassifier(
-        criterion="entropy", max_depth=100, min_samples_split=2
-    )
+    sklearn_model = tree.DecisionTreeClassifier(criterion="entropy", max_depth=100)
     sklearn_model.fit(X_train, y_train)
     sklearn_y_pred = sklearn_model.predict(X_test)
 
